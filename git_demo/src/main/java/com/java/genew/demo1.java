d8bd4fac2b8f060c2c7798b64359f8e39d604521
MengAo <mengao@genew.com>
2022-07-25 14:49:02 +0800

    第二次提交的文件内容

--- git_demo/src/main/java/com/java/genew/demo1.java
+++ git_demo/src/main/java/com/java/genew/demo1.java
@@ -3,5 +3,6 @@ package com.java.genew;
 public class demo1 {
     public static void main(String[] args) {
         System.out.println("git demo 1");
+        System.out.println("git demo 2 添加的文件内容");
     }
 }
