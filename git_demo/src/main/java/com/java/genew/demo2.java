a4328fc1d99ce28d1504cc4339e825c276aa3ce2
MengAo <mengao@genew.com>
2022-07-25 14:55:09 +0800

    hot_fix第二次提交的文件内容

--- git_demo/src/main/java/com/java/genew/demo2.java
+++ git_demo/src/main/java/com/java/genew/demo2.java
@@ -3,5 +3,6 @@ package com.java.genew;
 public class demo2 {
     public static void main(String[] args) {
         System.out.println("修改了输出内容");
+        System.out.println("这是分支添加的内容");
     }
 }
