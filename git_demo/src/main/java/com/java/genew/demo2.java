3d8c23b9b4cadbf66188ed6d518672be8aa363c2
MengAo <mengao@genew.com>
2022-07-25 14:55:59 +0800

    主干第三次提交的文件内容

--- git_demo/src/main/java/com/java/genew/demo2.java
+++ git_demo/src/main/java/com/java/genew/demo2.java
@@ -3,5 +3,6 @@ package com.java.genew;
 public class demo2 {
     public static void main(String[] args) {
         System.out.println("修改了输出内容");
+        System.out.println("这是主干添加的内容");
     }
 }
